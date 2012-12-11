f6ef123362cbf5471d846dcd624fc7d7db62e2ae
Vojtech Bocek <vbocek@gmail.com>
2012-12-12 00:11:09 +0100

    Move boot_img_hdr structure to it's own file

--- multirom.h
+++ multirom.h
@@ -2,6 +2,7 @@
 #define MULTIROM_H
 
 #include <pthread.h>
+#include "boot_img_hdr.h"
 
 enum
 {
@@ -67,8 +68,6 @@ struct multirom_status
     char *curr_rom_part;
 };
 
-typedef struct boot_img_hdr boot_img_hdr;
-
 int multirom(void);
 int multirom_find_base_dir(void);
 void multirom_emergency_reboot(void);
@@ -110,61 +109,5 @@ int multirom_scan_partition_for_roms(struct multirom_status *s, struct usb_parti
 struct usb_partition *multirom_get_partition(struct multirom_status *s, char *uuid);
 struct usb_partition *multirom_get_data_partition(struct multirom_status *s);
 int multirom_path_exists(char *base, char *filename);
-
-/*
-** +-----------------+ 
-** | boot header     | 1 page
-** +-----------------+
-** | kernel          | n pages  
-** +-----------------+
-** | ramdisk         | m pages  
-** +-----------------+
-** | second stage    | o pages
-** +-----------------+
-**
-** n = (kernel_size + page_size - 1) / page_size
-** m = (ramdisk_size + page_size - 1) / page_size
-** o = (second_size + page_size - 1) / page_size
-**
-** 0. all entities are page_size aligned in flash
-** 1. kernel and ramdisk are required (size != 0)
-** 2. second is optional (second_size == 0 -> no second)
-** 3. load each element (kernel, ramdisk, second) at
-**    the specified physical address (kernel_addr, etc)
-** 4. prepare tags at tag_addr.  kernel_args[] is
-**    appended to the kernel commandline in the tags.
-** 5. r0 = 0, r1 = MACHINE_TYPE, r2 = tags_addr
-** 6. if second_size != 0: jump to second_addr
-**    else: jump to kernel_addr
-*/
-
-#define BOOT_MAGIC "ANDROID!"
-#define BOOT_MAGIC_SIZE 8
-#define BOOT_NAME_SIZE 16
-#define BOOT_ARGS_SIZE 512
-
-struct boot_img_hdr
-{
-    unsigned char magic[BOOT_MAGIC_SIZE];
-
-    unsigned kernel_size;  /* size in bytes */
-    unsigned kernel_addr;  /* physical load addr */
-
-    unsigned ramdisk_size; /* size in bytes */
-    unsigned ramdisk_addr; /* physical load addr */
-
-    unsigned second_size;  /* size in bytes */
-    unsigned second_addr;  /* physical load addr */
-
-    unsigned tags_addr;    /* physical addr for kernel tags */
-    unsigned page_size;    /* flash page size we assume */
-    unsigned unused[2];    /* future expansion: should be 0 */
-
-    unsigned char name[BOOT_NAME_SIZE]; /* asciiz product name */
-    
-    unsigned char cmdline[BOOT_ARGS_SIZE];
-
-    unsigned id[8]; /* timestamp / checksum / sha1 / etc */
-};
 
 #endif
