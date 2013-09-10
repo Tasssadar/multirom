442e86fb88f02a65e413601f6e0052419a85cdbc
Vojtech Bocek <vbocek@gmail.com>
2013-09-10 19:51:07 +0200

    Backport "Add versioning schema for device-specific fixes"
    Conflicts:
    	themes/multirom_ui_1200x1920.c
    	themes/multirom_ui_1920x1200.c
    	version.h

--- version.h
+++ version.h
@@ -2,4 +2,7 @@
 #define VERSION_H
     #define VERSION_MULTIROM 12
     #define VERSION_TRAMPOLINE 4
+
+    // For device-specific fixes. Use letters, the version will then be like "12a"
+    #define VERSION_DEV_FIX ""
 #endif
