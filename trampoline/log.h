cef760c5b73a34976467ca4c10b502e5295be313
Vojtech Bocek <vbocek@gmail.com>
2013-11-26 21:51:59 +0100

    Restore build

--- trampoline/log.h
+++ trampoline/log.h
@@ -4,6 +4,7 @@
 #include <cutils/klog.h>
 
 #define ERROR(x...)   KLOG_ERROR("trampoline", x)
+#define NOTICE(x...)  KLOG_NOTICE("trampoline", x)
 #define INFO(x...)    KLOG_INFO("trampoline", x)
 
 #endif
