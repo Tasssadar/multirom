8731ad1e1f4804b00f498aab094de2f32e346206
Vojtech Bocek <vbocek@gmail.com>
2014-01-03 16:26:02 +0100

    [m20] Bump version

--- version.h
+++ version.h
@@ -17,7 +17,7 @@
 
 #ifndef VERSION_H
 #define VERSION_H
-    #define VERSION_MULTIROM 19
+    #define VERSION_MULTIROM 20
     #define VERSION_TRAMPOLINE 10
 
     // For device-specific fixes. Use letters, the version will then be like "12a"
