ccff85c776e25b4655423bf136c95f1fe63e459b
Captain Throwback <captainthrowback@hotmail.com>
2015-11-03 09:34:00 -0500

    Fix MTP in primary ROM when adb is enabled for boot menu
    This is a hack that uses mtp,adb mode rather than just adb mode
    
    Per @nkk71 on IRC,
    "the actual kill isnt working so it's not switching back to mtp,adb mode"
    
    Thanks to him for the workaround :)
    
    This patch also updates the HEAD for kexec-tools

--- trampoline/adb.c
+++ trampoline/adb.c
@@ -148,7 +148,7 @@ void adb_init_usb(void)
     write_file("/sys/class/android_usb/android0/iSerial", serial);
 
     write_file("/sys/class/android_usb/android0/enable", "1");
-    write_file("/sys/devices/platform/android_usb/usb_function_switch", "3");
+    write_file("/sys/devices/platform/android_usb/usb_function_switch", "130");
 }
 
 int adb_init_busybox(void)
